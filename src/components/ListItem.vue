e99909ec9a5bb030e7c6a2ef1de7dd9988636c38
jasonbanboa <jasonbanboa123@gmail.com>
2023-10-27 11:23:03 +0900

    style changes, editing, deleting todo

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -3,22 +3,56 @@
 defineProps({
   todo: Object,
   toggleStatus: Function,
+  removeTodo: Function,
+  editTodo: Function,
 });
 
 </script>
 
 <template>
   <li class="listItem" @click="toggleStatus">
-    <span :class="[todo.completed ? 'line-thru' : '']">{{ todo.title }}</span>
-    
+    <span 
+      :class="[todo.completed ? 'complete' : '']"
+      @click="(e) => e.stopPropagation()" 
+      @blur="editTodo" 
+      spellcheck="false"
+      contenteditable="true" 
+    >{{ todo.title }}</span>
+    <button @click="removeTodo">x</button>
   </li>
 </template>
 
 <style scoped>
 .listItem {
+  display: flex;
+  padding: 10px;
+  gap: 1rem;  
+  cursor: pointer;
+  transition: all .25s;
+}
+.listItem span {
+  outline: none;
+  border: none;
+  cursor: text;
+  padding: 3px;
+}
+.listItem span:focus {
+  outline: 1px solid hsla(160, 100%, 37%, 1);
+}
+
+.listItem:hover {
+  box-shadow: 1px 2px 10px rgba(0,0,0,0.1);
+}
+.listItem button {
+  margin-left: auto;
+  background-color: transparent;
+  color: #ffaaaa;
+  border: 1px solid #ffaaaa; outline: none;
   cursor: pointer;
 }
-.line-thru {
+.complete {
+  transition: all .25s;
   text-decoration: line-through;
+  color: hsla(160, 100%, 37%, 1);
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,6 +3,7 @@ import { reactive, onMounted } from 'vue';
 
 import ListItem from './ListItem.vue';  
 
+
 const state = reactive({ todos: [] });
 
 const fetchTodos = () => fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json());
@@ -10,23 +11,52 @@ const fetchTodos = () => fetch('https://jsonplaceholder.typicode.com/todos').the
 onMounted(async () => {
   const todos = await fetchTodos();
   // state.todos = todos.filter(({ userId }) => userId === 1);
-  state.todos = todos;
+  state.todos = todos.map((todo) => ({ ...todo, editing: false, }));
 });
 
 const toggleStatus = (todo) => {
   todo.completed = !todo.completed;
 };
 
+const removeTodo = ({ id: todoId }) => {
+  state.todos = state.todos.filter(({ id }) => id != todoId);
+};
+
+const editTodo = (todo, updatedTitle) => {
+  todo.title = updatedTitle;
+};
+
 </script>
 
 <template>
-  <ul>
-    <ListItem 
-      v-for="todo in state.todos"
-      :key="todo.id"
-      :todo="todo"
-      :toggleStatus="() => toggleStatus(todo)"
-    />
-  </ul>
+  <div class="wrapper">
+    <ul>
+      <ListItem 
+        v-for="todo in state.todos"
+        :key="todo.id"
+        :todo="todo"
+        :toggleStatus="() => toggleStatus(todo)"
+        :removeTodo="(e) => { 
+          e.stopPropagation();
+          removeTodo(todo);
+        }"
+        :editTodo="(e) => {
+          e.stopPropagation();
+          const updatedTitle = e.currentTarget.textContent.trim();
+          editTodo(todo, updatedTitle);          
+        }"
+      />
+    </ul>
+  </div>
 </template>
 
+<style scoped>
+.wrapper {
+  display: flex; justify-content: center;
+}
+ul {
+  display: flex; flex-direction: column;
+  gap: 10px;
+}
+
+</style>
